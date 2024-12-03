dfae2f91681c5449432a8b87a049a8d5a7eae29b
Thbispo <thiagosouza0100@gmail.com>
2024-12-03 12:04:35 -0300

    Formulário.Aula2.1

---
+++ Formulário.Aula3.0/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace Formulário.Aula3._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox_Genero = new System.Windows.Forms.TextBox();
+            this.textBox_Ano = new System.Windows.Forms.TextBox();
+            this.textBox_Avaliacao = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button_Cadastrar = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button_limpar = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.textBox_Titulo = new System.Windows.Forms.TextBox();
+            this.label_Titulo = new System.Windows.Forms.Label();
+            this.label_Avaliacao = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox_Genero
+            // 
+            this.textBox_Genero.Location = new System.Drawing.Point(118, 144);
+            this.textBox_Genero.Name = "textBox_Genero";
+            this.textBox_Genero.Size = new System.Drawing.Size(302, 20);
+            this.textBox_Genero.TabIndex = 1;
+            // 
+            // textBox_Ano
+            // 
+            this.textBox_Ano.Location = new System.Drawing.Point(118, 203);
+            this.textBox_Ano.Name = "textBox_Ano";
+            this.textBox_Ano.Size = new System.Drawing.Size(302, 20);
+            this.textBox_Ano.TabIndex = 2;
+            // 
+            // textBox_Avaliacao
+            // 
+            this.textBox_Avaliacao.Location = new System.Drawing.Point(118, 264);
+            this.textBox_Avaliacao.Name = "textBox_Avaliacao";
+            this.textBox_Avaliacao.Size = new System.Drawing.Size(302, 20);
+            this.textBox_Avaliacao.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(216, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Cadastro de Filmes";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 24);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Título";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 24);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Genero";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 203);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 24);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Ano";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 260);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(106, 48);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Avaliação \r\n     1-5";
+            // 
+            // button_Cadastrar
+            // 
+            this.button_Cadastrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Cadastrar.Location = new System.Drawing.Point(683, 399);
+            this.button_Cadastrar.Name = "button_Cadastrar";
+            this.button_Cadastrar.Size = new System.Drawing.Size(105, 39);
+            this.button_Cadastrar.TabIndex = 9;
+            this.button_Cadastrar.Text = "Cadastro";
+            this.button_Cadastrar.UseVisualStyleBackColor = true;
+            this.button_Cadastrar.Click += new System.EventHandler(this.button_Cadastrar_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(455, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(185, 25);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Título e Avaliação";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(460, 45);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(180, 223);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // button_limpar
+            // 
+            this.button_limpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_limpar.Location = new System.Drawing.Point(683, 354);
+            this.button_limpar.Name = "button_limpar";
+            this.button_limpar.Size = new System.Drawing.Size(105, 39);
+            this.button_limpar.TabIndex = 12;
+            this.button_limpar.Text = "Limpar";
+            this.button_limpar.UseVisualStyleBackColor = true;
+            this.button_limpar.Click += new System.EventHandler(this.button_limpar_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(460, 264);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(180, 25);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 14;
+            this.pictureBox2.TabStop = false;
+            // 
+            // textBox_Titulo
+            // 
+            this.textBox_Titulo.Location = new System.Drawing.Point(118, 86);
+            this.textBox_Titulo.Name = "textBox_Titulo";
+            this.textBox_Titulo.Size = new System.Drawing.Size(302, 20);
+            this.textBox_Titulo.TabIndex = 16;
+            // 
+            // label_Titulo
+            // 
+            this.label_Titulo.AutoSize = true;
+            this.label_Titulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_Titulo.Location = new System.Drawing.Point(683, 45);
+            this.label_Titulo.Name = "label_Titulo";
+            this.label_Titulo.Size = new System.Drawing.Size(0, 18);
+            this.label_Titulo.TabIndex = 17;
+            // 
+            // label_Avaliacao
+            // 
+            this.label_Avaliacao.AutoSize = true;
+            this.label_Avaliacao.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_Avaliacao.Location = new System.Drawing.Point(683, 86);
+            this.label_Avaliacao.Name = "label_Avaliacao";
+            this.label_Avaliacao.Size = new System.Drawing.Size(0, 18);
+            this.label_Avaliacao.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label_Avaliacao);
+            this.Controls.Add(this.label_Titulo);
+            this.Controls.Add(this.textBox_Titulo);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button_limpar);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button_Cadastrar);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_Avaliacao);
+            this.Controls.Add(this.textBox_Ano);
+            this.Controls.Add(this.textBox_Genero);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Cadastro de Filmes";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox_Genero;
+        private System.Windows.Forms.TextBox textBox_Ano;
+        private System.Windows.Forms.TextBox textBox_Avaliacao;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button_Cadastrar;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button_limpar;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox textBox_Titulo;
+        private System.Windows.Forms.Label label_Titulo;
+        private System.Windows.Forms.Label label_Avaliacao;
+    }
+}
+
---
+++ Formulário.Aula3.0/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Formulário.Aula3._0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Cadastrar_Click(object sender, EventArgs e)
+        {
+            string nomeTitulo = "";
+            string nomeGenero = "";
+            int AnoFilme;
+            int AvaliacaoFilme;
+            nomeTitulo = textBox_Titulo.Text;
+            nomeGenero = textBox_Genero.Text;
+            AnoFilme = int.Parse(textBox_Ano.Text);
+            AvaliacaoFilme = int.Parse(textBox_Avaliacao.Text);
+            pictureBox1.Visible = true;
+
+            label_Titulo.Text = nomeTitulo;
+            label_Avaliacao.Text = AvaliacaoFilme.ToString();
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_limpar_Click(object sender, EventArgs e)
+        {
+            textBox_Titulo.Text = string.Empty;
+            textBox_Genero.Text = string.Empty;
+            textBox_Ano.Text = string.Empty;
+            textBox_Avaliacao.Text = string.Empty;
+            pictureBox1 .Visible = false;
+        }
+
+        
+    }
+       
+
+}
+
