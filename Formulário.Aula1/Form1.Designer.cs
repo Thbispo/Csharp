fdaa6bbb4e6d380e2e51d761a724e8895970d1ae
Thbispo <thiagosouza0100@gmail.com>
2024-12-03 12:06:07 -0300

    Caua foi a praia

---
+++ Formulário.Aula1/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Formulário.Aula1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(377, 245);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(276, 149);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Mensagem para Caua";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label2.Location = new System.Drawing.Point(290, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(276, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "O Caua foi viajar e vai ficar o dia todo no banheiro";
+            // 
+            // Form1
+            // 
+            this.BackgroundImage = global::Formulário.Aula1.Properties.Resources.family_guy_peter_no_vaso_sanitario_tcntoe8aopkvltif;
+            this.ClientSize = new System.Drawing.Size(899, 475);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Formulário.Aula1/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Formulário.Aula1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Cuidado Kauan com oque voce come !!!"," Perigo!!!",
+            MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+
+    }
+}
